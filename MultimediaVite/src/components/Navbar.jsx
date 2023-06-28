917f59f997bcaddcbf8825c914933411c67bd342
justindavidcalle <justincalle@outlook.de>
2023-06-28 20:15:35 +0200

    Navbar erneuert

--- MultimediaVite/src/components/Navbar.jsx
+++ MultimediaVite/src/components/Navbar.jsx
@@ -1,16 +1,31 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { useState } from "react"
+import '../css/navbar.css'
 
 export default function Navbar(){
 
+    const [clicked, setClicked] = useState(false)
+    
+    const handleClick = () =>{
+        setClicked(!clicked)
+    }
+
     return(
         <>
-        <nav className="nav">
-            <Link to="/" className="site-title">Site Name</Link>
-            <ul>
-                <CustomLink to="/sales">Sales</CustomLink>
-                <CustomLink to="/about">About</CustomLink>
-                <CustomLink to="/imprint">Imprint</CustomLink>
-                <CustomLink to="/contact">Contact</CustomLink>
+        <nav className="NavbarItems">
+            <h1 className="navbar-logo">Trippy</h1>
+
+            <div className="menu-icons" onClick={handleClick}>
+                <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+            </div>
+
+            <ul className={clicked ? "nav-menu active" : "nav-menu"}>
+                <Link to="/" className="nav-links"><i class="fa-solid fa-house"></i>Home</Link>
+                <CustomLink to="/sales" className="nav-links">Sales</CustomLink>
+                <CustomLink to="/about" className="nav-links">About</CustomLink>
+                <CustomLink to="/imprint" className="nav-links">Imprint</CustomLink>
+                <CustomLink to="/contact" className="nav-links">Contact</CustomLink>
+                <button>Sign up</button>
             </ul>
         </nav>
         </>
