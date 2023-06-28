359ff086b536e038d9cea43c3bbc4822f35e2151
justindavidcalle <justincalle@outlook.de>
2023-06-28 21:09:33 +0200

    Header erstellt und in Landingpage eingefügt

--- MultimediaVite/src/components/Hero.jsx
+++ MultimediaVite/src/components/Hero.jsx
@@ -1,11 +1,20 @@
+import { Link } from 'react-router-dom'
 import '../css/hero.css'
+import züriWappen from '../images/ZüriWappen.jpg'
 
-export default function Hero(){
+export default function Hero(props){
     
+
     return(
         <>
-        <div className='hero'>
-            <img src="" alt="HerpImg" />
+        <div className={props.cName}>
+            <img src={props.heroImg} alt="HeroImg" />
+
+            <div className='hero-text'>
+                <h1>{props.title}</h1>
+                <p>{props.text}</p>
+                <Link to={props.url} className={props.btnClass}>{props.buttonText}</Link>
+            </div>
         </div>
         </>
     )
--- MultimediaVite/src/sites/Landingpage.jsx
+++ MultimediaVite/src/sites/Landingpage.jsx
@@ -1,11 +1,21 @@
+import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
+import züriWappen from '../images/ZüriWappen.jpg'
 
 export default function Landingpage(){
 
     return(
         <>
-        <Navbar/>
-        <h1>Landpage</h1>
+        <Navbar />
+        <Hero 
+            cName="hero"
+            heroImg={züriWappen}
+            title="Your Story Your Journey"
+            text="Choose Your Favourite Destination"
+            buttonText="Travel Plan"
+            url="/"
+            btnClass="show"
+        />
         </>
     )
 }
