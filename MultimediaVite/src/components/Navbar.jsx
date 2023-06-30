1d5f834e7b9e7e007b0236c1a50d9d5d0ac405f4
justindavidcalle <justincalle@outlook.de>
2023-06-30 05:10:21 +0200

    Galerie erstellt

--- MultimediaVite/src/components/Navbar.jsx
+++ MultimediaVite/src/components/Navbar.jsx
@@ -22,9 +22,9 @@ export default function Navbar(){
 
             <ul className={clicked ? "nav-menu active" : "nav-menu"}>
                 <CustomLink to="/sales" className="nav-links"><i class="fa-solid fa-map"></i>Touren</CustomLink>
-                <CustomLink to="/about" className="nav-links"><i class="fa-solid fa-circle-info"></i>About</CustomLink>
-                <CustomLink to="/contact" className="nav-links"><i class="fa-solid fa-user"></i>Contact</CustomLink>
-                <CustomLink to="/galery" className="nav-links">Galery</CustomLink>
+                <CustomLink to="/about" className="nav-links"><i class="fa-solid fa-circle-info"></i>Über Uns</CustomLink>
+                <CustomLink to="/contact" className="nav-links"><i class="fa-solid fa-user"></i>Kontakt</CustomLink>
+                <CustomLink to="/galery" className="nav-links">Galerie</CustomLink>
                 <button>Sign up</button>
             </ul>
         </nav>
