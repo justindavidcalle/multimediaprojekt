11f19cdac6dba43da5f0e0262db41cb35915dece
justindavidcalle <justincalle@outlook.de>
2023-06-30 05:22:58 +0200

    Korrekturen

--- MultimediaVite/src/components/Trip.jsx
+++ MultimediaVite/src/components/Trip.jsx
@@ -15,19 +15,19 @@ export default function Trip(){
                 <TripData 
                     image={gucci}
                     heading="Shoppingreise"
-                    text="Shoppen bis zum Umfallen in Zürich! Mit ihren Luxusboutiquen in der Bahnhofstraße, der trendigen Mode im Kreis 5 und den pulsierenden Märkten wie dem Freitagsturm ist diese Schweizer Stadt ein Paradies für Shopping-Fans. Entdecken Sie Schweizer Uhren, stilvolle Mode und lokales Kunsthandwerk bei einem Streifzug durch die charmanten Straßen von Zürich."
+                    text="Shoppen bis zum Umfallen in Zürich! Mit ihren Luxusboutiquen in der Bahnhofstraße, der trendigen Mode im Kreis 5 und den pulsierenden Märkten wie dem Freitagsturm ist diese Schweizer Stadt ein Paradies für Shopping-Fans. Entdecken Sie Schweizer Uhren, stilvolle Mode und lokales Kunsthandwerk bei einem Streifzug durch die charmanten Straßen von Zürich. (200.- 3xTage)"
                 />
 
                 <TripData 
                     image={antikerHydrant}
                     heading="Geschichtsreise"
-                    text="Entdecken Sie Zürichs faszinierende Geschichte auf unserer Geschichtstour! Tauchen Sie ein in vergangene Zeiten, begleitet von unseren Guides. Erleben Sie bedeutende Ereignisse, historische Stätten und die einzigartige Atmosphäre der Stadt. Buchen Sie jetzt Ihre Tour und entdecken Sie Zürichs Geschichte!"
+                    text="Entdecken Sie Zürichs faszinierende Geschichte auf unserer Geschichtstour! Tauchen Sie ein in vergangene Zeiten, begleitet von unseren Guides. Erleben Sie bedeutende Ereignisse, historische Stätten und die einzigartige Atmosphäre der Stadt. Buchen Sie jetzt Ihre Tour und entdecken Sie Zürichs Geschichte!(200.- 5xTage)"
                 />
 
                 <TripData 
                     image={see}
                     heading="Wassertour"
-                    text="Erleben Sie den Zauber des Zürichsees auf unserer Seetour! Genießen Sie die atemberaubende Schönheit des Sees und entdecken Sie seine malerischen Uferstädte. Unsere erfahrenen Guides begleiten Sie auf einer unvergesslichen Reise entlang des Sees und teilen faszinierende Geschichten über seine Geschichte und Kultur. Buchen Sie jetzt Ihre Tour und lassen Sie sich vom Zürichsee verzaubern!"
+                    text="Erleben Sie den Zauber des Zürichsees auf unserer Seetour! Genießen Sie die atemberaubende Schönheit des Sees und entdecken Sie seine malerischen Uferstädte. Unsere erfahrenen Guides begleiten Sie auf einer unvergesslichen Reise entlang des Sees und teilen faszinierende Geschichten über seine Geschichte und Kultur. Buchen Sie jetzt Ihre Tour und lassen Sie sich vom Zürichsee verzaubern!(150.- 3xTage)"
                 />
 
             </div>
